ba0263924a913bcef9cb7e25906608568d867277
Tim Northover <tnorthover@apple.com>
2015-12-02 18:12:57 +0000

    AArch64: use ldxp/stxp pair to implement 128-bit atomic loads.
    The ARM ARM is clear that 128-bit loads are only guaranteed to have been atomic
    if there has been a corresponding successful stxp. It's less clear for AArch32, so
    I'm leaving that alone for now.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@254524 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Target/Hexagon/HexagonISelLowering.cpp
+++ lib/Target/Hexagon/HexagonISelLowering.cpp
@@ -2504,7 +2504,7 @@ TargetLowering::AtomicExpansionKind
 HexagonTargetLowering::shouldExpandAtomicLoadInIR(LoadInst *LI) const {
   // Do not expand loads and stores that don't exceed 64 bits.
   return LI->getType()->getPrimitiveSizeInBits() > 64
-             ? AtomicExpansionKind::LLSC
+             ? AtomicExpansionKind::LLOnly
              : AtomicExpansionKind::None;
 }
 

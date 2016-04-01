da67bff217ca160c5073fb7ef4fbf3054a4f2ac4
Akira Hatanaka <ahatanaka@apple.com>
2016-04-01 00:34:39 +0000

    [LoopVectorize] Don't unconditionally print vectorization diagnostics when compiling with LTO.
    r244523 a new class DiagnosticInfoOptimizationRemarkAnalysisAliasing for
    optimization analysis remarks related to pointer aliasing without
    guarding it in isDiagnosticEnabled in LLVMContext.cpp. This caused the
    diagnostic message to be printed unconditionally when compiling with
    LTO.
    
    This commit cleans up isDiagnosticEnabled and makes sure all the
    vectorization optimization remarks are guarded.
    
    rdar://problem/25382153
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@265084 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit 5fe8a3c994b768821225451397c4cabdb23e3686)

--- include/llvm/IR/DiagnosticInfo.h
+++ include/llvm/IR/DiagnosticInfo.h
@@ -59,6 +59,8 @@ enum DiagnosticKind {
   DK_OptimizationRemarkAnalysisFPCommute,
   DK_OptimizationRemarkAnalysisAliasing,
   DK_OptimizationFailure,
+  DK_FirstRemark = DK_OptimizationRemark,
+  DK_LastRemark = DK_OptimizationFailure,
   DK_MIRParser,
   DK_FirstPluginKind
 };
@@ -293,6 +295,11 @@ public:
   const Function &getFunction() const { return Fn; }
   const DebugLoc &getDebugLoc() const { return DLoc; }
   const Twine &getMsg() const { return Msg; }
+
+  static bool classof(const DiagnosticInfo *DI) {
+    return DI->getKind() >= DK_FirstRemark &&
+           DI->getKind() <= DK_LastRemark;
+  }
 
 private:
   /// Name of the pass that triggers this report. If this matches the
